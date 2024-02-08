31660df51f860c0bba236aa2e881033ee05c3a7b
Parralol <110953563+Parralol@users.noreply.github.com>
2024-02-08 14:15:46 -0500

    Add files via upload

---
+++ TALLER2_AREP/src/main/java/edu/escuelaing/arem/ASE/app/HttpContext.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arem.ASE.app;
+
+public class HttpContext {
+    
+    public static final String getHtml(){
+        return "text/html" + "\r\n";
+    }
+    public static final String getImg(){
+        return "image/webp";
+    }
+    public static final String getCss(){
+        return "text/css";
+    }
+
+    public static final String getJs(){
+        return "text/javascript";
+    }
+
+
+}
